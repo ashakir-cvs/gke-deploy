c1538b811baa9ed27eab8bf4bc506f150722a2d4
Tobe Anyigbo <tobe.anyigbo@cvshealth.com>
2024-07-17 21:35:42 -0500

    initial commit

---
+++ src/main/java/org/example/gkedeploy/GkeDeployApplication.java
@@ -0,0 +1,13 @@
+package org.example.gkedeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GkeDeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GkeDeployApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/gkedeploy/GkeDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gkedeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GkeDeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
