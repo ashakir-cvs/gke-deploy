6e642aeedea9ae610ad941d0d6f30eda2f79bec1
Tobe Anyigbo <tobe.anyigbo@cvshealth.com>
2024-07-19 16:56:35 -0500

    print username

--- src/main/java/org/example/gkedeploy/KafkaRecordListener.java
+++ src/main/java/org/example/gkedeploy/KafkaRecordListener.java
@@ -2,6 +2,7 @@ package org.example.gkedeploy;
 
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +11,12 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class KafkaRecordListener {
 
+    @Value("${username}")
+    String username;
+
     @KafkaListener(topics = "topic_01")
     public void consumeKafkaMessage(String message) {
         log.warn(message);
+        log.debug(username);
     }
 }
