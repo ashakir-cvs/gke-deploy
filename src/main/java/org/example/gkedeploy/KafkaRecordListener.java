b2665a67fc6882a79986282342512af00ed52f6a
Tobe Anyigbo <tobe.anyigbo@cvshealth.com>
2024-07-19 00:14:43 -0500

    log message as warn

--- src/main/java/org/example/gkedeploy/KafkaRecordListener.java
+++ src/main/java/org/example/gkedeploy/KafkaRecordListener.java
@@ -1,15 +1,17 @@
 package org.example.gkedeploy;
 
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 @NoArgsConstructor
+@Slf4j
 public class KafkaRecordListener {
 
     @KafkaListener(topics = "topic_01")
     public void consumeKafkaMessage(String message) {
-        System.out.println(message);
+        log.warn(message);
     }
 }
