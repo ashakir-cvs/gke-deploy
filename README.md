a29c71fb1adbb580ecf76c8cd4d13215d07cc5ec
Tobe Anyigbo <tobe.anyigbo@cvshealth.com>
2024-07-21 00:03:25 -0500

    use existing secret names

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Setup Deployment To Retrieve Secrets Using Google Secret Manager Add-on
+
+TODO
+- Test usability with roles assigned GKE Service Accounts
+- Test method of parsing json object or multi line secrets
--- src/main/java/org/example/gkedeploy/KafkaRecordListener.java
+++ src/main/java/org/example/gkedeploy/KafkaRecordListener.java
@@ -2,7 +2,6 @@ package org.example.gkedeploy;
 
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +10,8 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class KafkaRecordListener {
 
-    @Value("${username}")
-    String username;
-
     @KafkaListener(topics = "topic_01")
     public void consumeKafkaMessage(String message) {
         log.warn(message);
-        log.warn(username);
     }
 }
